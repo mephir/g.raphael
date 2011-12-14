dc2b328deae5c735807ced4f35cb17f9f53c1cfa
Pawel Wilk <pawel@wilk.pro>
2011-12-14 12:20:32 +0100

    graph with labels

--- mp.bar.js
+++ mp.bar.js
@@ -7,25 +7,24 @@
 Raphael.fn.g.mpbarchart = function (x, y, width, height, values, opts) {
   opts = opts || {};
   var paper = this,
-    chart = this.set(),
     max = Math.max.apply(Math, values),
-    colors = opts.colors || ['0-#f00-#fff', '0-#0f0-#fff', '0-#00f-#fff'];//this.g.colors,
-    len = values.length,
+    colors = opts.colors || ['0-#eee-#f00', '0-#eee-#0f0', '0-#eee-#00f'];//this.g.colors,
     total = 0,
     percents = [],
     lengths = [],
     paths = [];
 
     var barMaxLength = opts.barMaxLength || 190,
-        barHeight = opts.barHeight || 24,
-        barMargin = opts.barMargin || 4,
+        barHeight = opts.barHeight || 26,
+        barMargin = opts.barMargin || 8,
         barsMarginTop = opts.barsMarginTop || 8,
         barsMarginLeft = opts.barsMarginLeft || 0,
         triangleHeight = opts.triangleHeight || 12;
+        valueSize = opts.valueSize || Math.round(barHeight * 0.9),
+        valueColor = opts.valueColor || '#e2ebbe';
 
     paper.rect(0,0,width,height).attr({fill: "#000"}); //helper in background, to remove
 
-    //calculations
     for(var x in values)
     {
       total += values[x];
@@ -33,17 +32,21 @@ Raphael.fn.g.mpbarchart = function (x, y, width, height, values, opts) {
 
     for(var x in values)
     {
-      percents.push(Math.round((values[x] / total)*100)/100);
+      percents.push(Math.round((values[x] / total)*100));
       lengths.push(Math.round((values[x] * barMaxLength) / max));
     }
+
     var startY = barsMarginTop;
     var rectLength,
+        textMarginLeft,
+        printPath,
+        textNode,
         trianglePickY = Math.round(barHeight / 2),
-        printPath;
+        textMarginTop = Math.round((barHeight - valueSize) / 2);
 
     for(var x in lengths)
     {
-      rectLength = lengths[x] - triangleHeight;
+      rectLength = lengths[x] - triangleHeight + barsMarginLeft;
 
       printPath = ['M',barsMarginLeft,startY,
         'L',rectLength,startY,
@@ -51,10 +54,11 @@ Raphael.fn.g.mpbarchart = function (x, y, width, height, values, opts) {
         'L',rectLength,(startY + barHeight),
         'L',barsMarginLeft,(startY + barHeight),'Z'];
 
-      paper.path(printPath).attr('fill', '0-#f00-#fff');
-
+      paper.path(printPath).attr('fill', colors[ x % colors.length]);
+      textNode = paper.text(barsMarginLeft, startY + textMarginTop + y, percents[x] + '%');
+      textNode.attr('x', (rectLength - textNode.getBBox().width)).attr({'font-size' : valueSize, 'fill' : valueColor});
       startY = startY + barHeight + barMargin;
     }
 
-    console.log(max, total, percents, lengths);
+    console.log(textMarginTop);
 };
