830a5d2004d9bf53341f51551021e0506d8dd161
Pawel Wilk <pawel@wilk.pro>
2011-12-14 13:05:12 +0100

    final chart

--- mp.bar.js
+++ mp.bar.js
@@ -8,57 +8,63 @@ Raphael.fn.g.mpbarchart = function (x, y, width, height, values, opts) {
   opts = opts || {};
   var paper = this,
     max = Math.max.apply(Math, values),
-    colors = opts.colors || ['0-#eee-#f00', '0-#eee-#0f0', '0-#eee-#00f'];//this.g.colors,
+    colors = opts.colors || ['0-#eee-#f00', '0-#eee-#0f0', '0-#eee-#00f'];
     total = 0,
     percents = [],
     lengths = [],
     paths = [];
 
-    var barMaxLength = opts.barMaxLength || 190,
-        barHeight = opts.barHeight || 26,
-        barMargin = opts.barMargin || 8,
-        barsMarginTop = opts.barsMarginTop || 8,
-        barsMarginLeft = opts.barsMarginLeft || 0,
-        triangleHeight = opts.triangleHeight || 12;
-        valueSize = opts.valueSize || Math.round(barHeight * 0.9),
-        valueColor = opts.valueColor || '#e2ebbe';
+  var barMaxLength = opts.barMaxLength || 190,
+      barHeight = opts.barHeight || 26,
+      barMargin = opts.barMargin || 8,
+      barsMarginTop = opts.barsMarginTop || 8,
+      barsMarginLeft = opts.barsMarginLeft || 0,
+      triangleHeight = opts.triangleHeight || 12;
+      valueSize = opts.valueSize || Math.round(barHeight * 0.9),
+      valueColor = opts.valueColor || '#ffffff',
+      valueColorOutside = opts.valueColorOutside || '#444444';
 
-    paper.rect(0,0,width,height).attr({fill: "#000"}); //helper in background, to remove
+  for(var x in values)
+  {
+    total += values[x];
+  }
+  for(var x in values)
+  {
+    percents.push(Math.round((values[x] / total)*100));
+    lengths.push(Math.round((values[x] * barMaxLength) / max));
+  }
 
-    for(var x in values)
+  var startY = barsMarginTop,
+      rectLength,
+      textMarginLeft,
+      printPath,
+      textNode,
+      trianglePickY = Math.round(barHeight / 2),
+      textMarginTop = Math.round((barHeight - valueSize) / 2);
+
+  for(var x in lengths)
+  {
+    rectLength = lengths[x] - triangleHeight + barsMarginLeft;
+
+    printPath = ['M',barsMarginLeft,startY,
+      'L',rectLength,startY,
+      'L',(triangleHeight + rectLength),(startY + trianglePickY),
+      'L',rectLength,(startY + barHeight),
+      'L',barsMarginLeft,(startY + barHeight),'Z'];
+    paper.path(printPath).attr('fill', colors[ x % colors.length]).attr('stroke', 'none');
+    textNode = paper.text(rectLength, startY + textMarginTop + y, percents[x] + '%').attr({'font-size' : valueSize, 'fill' : valueColor});
+    if(textNode.getBBox().width < rectLength)
     {
-      total += values[x];
+      textNode.attr('text-anchor', 'end');
     }
-
-    for(var x in values)
+    else
     {
-      percents.push(Math.round((values[x] / total)*100));
-      lengths.push(Math.round((values[x] * barMaxLength) / max));
+      textNode.attr({
+        'x' : (rectLength + triangleHeight),
+        'fill' : valueColorOutside,
+        'text-anchor' : 'start'
+      });
     }
-
-    var startY = barsMarginTop;
-    var rectLength,
-        textMarginLeft,
-        printPath,
-        textNode,
-        trianglePickY = Math.round(barHeight / 2),
-        textMarginTop = Math.round((barHeight - valueSize) / 2);
-
-    for(var x in lengths)
-    {
-      rectLength = lengths[x] - triangleHeight + barsMarginLeft;
-
-      printPath = ['M',barsMarginLeft,startY,
-        'L',rectLength,startY,
-        'L',(triangleHeight + rectLength),(startY + trianglePickY),
-        'L',rectLength,(startY + barHeight),
-        'L',barsMarginLeft,(startY + barHeight),'Z'];
-
-      paper.path(printPath).attr('fill', colors[ x % colors.length]);
-      textNode = paper.text(barsMarginLeft, startY + textMarginTop + y, percents[x] + '%');
-      textNode.attr('x', (rectLength - textNode.getBBox().width)).attr({'font-size' : valueSize, 'fill' : valueColor});
-      startY = startY + barHeight + barMargin;
-    }
-
-    console.log(textMarginTop);
+    startY = startY + barHeight + barMargin;
+  }
 };
